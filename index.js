c42c32b30b692d6439b11b7ff0a43c0674585cb1
Todd Albert <todd@bocacode.com>
2022-10-17 11:54:56 -0400

    built a simple express api

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/test', (request, response) => {
+  response.send({ message: "😎🥰🙂🥲😍😌😎 It's all good, man! 😎😎😎" })
+})
+
+app.get('/', (request, response) => response.send({ message: 'Welcome to our first API' }))
+
+app.get('/error', (request, response) => {
+  response
+    .status(500)
+    .send({ message: 'Something went wrong', success: false })
+})
+
+app.listen(3030, () => console.log('Listening on http://localhost:3030 ...'))
